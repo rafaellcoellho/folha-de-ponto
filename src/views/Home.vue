27a2a0475813d75854bb5cc81587e268e8a52587
Rafael Coelho <rafaellcoellho@gmail.com>
2019-04-13 16:24:44 -0300

    Create table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,51 @@
 <template>
-  <h1>Opa</h1>
+  <v-card>
+
+    <v-card-title>
+      <h1>Folha de Pontos</h1>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Pesquisa"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+
+    <v-data-table
+      :headers="headers"
+      :items="records"
+      :search="search"
+      no-data-text="Sem pontos para mostrar" 
+    >
+      <template v-slot:items="props">
+        <td>{{ props.item.day }}</td>
+        <td>{{ props.item.name }}</td>
+        <td>{{ props.item.punchIn }}</td>
+        <td>{{ props.item.pauseStart }}</td>
+        <td>{{ props.item.pauseEnd }}</td>
+        <td>{{ props.item.punchOut }}</td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
 export default {
-  components: {
-
-  }
+  data () {
+    return {
+      search: '',
+      headers: [
+        { text: 'Dia', align: 'left', value: 'day' },
+        { text: 'Funcionário', value: 'name'},
+        { text: 'Entrada', value: 'punchIn', sortable: false },
+        { text: 'Inicio da Pausa', value: 'pauseStart', sortable: false },
+        { text: 'Retorno da Pausa', value: 'pauseEnd', sortable: false },
+        { text: 'Saída', value: 'punchOut', sortable: false }
+      ],
+      records: []
+    }
+  },
 }
 </script>
